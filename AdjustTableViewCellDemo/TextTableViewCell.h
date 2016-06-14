aaa177878bafb8174a9da9be17a459e3407f9800
coderYMS <yu6mengshi@sina.com>
2016-06-14 15:52:57 +0800

    demo finish

---
+++ AdjustTableViewCellDemo/TextTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TextTableViewCell.h
+//  AdjustTableViewCellDemo
+//
+//  Created by mac on 16/6/14.
+//  Copyright © 2016年 yms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextTableViewCell : UITableViewCell
+
+@end
---
+++ AdjustTableViewCellDemo/TextTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TextTableViewCell.m
+//  AdjustTableViewCellDemo
+//
+//  Created by mac on 16/6/14.
+//  Copyright © 2016年 yms. All rights reserved.
+//
+
+#import "TextTableViewCell.h"
+
+@implementation TextTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
